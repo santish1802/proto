25ae34462008a265990d2e5ffea465dc531eaf63
santish1802 <santiago497045@gmail.com>
2024-09-11 18:56:53 -0500

    x2

--- server.py
+++ server.py
@@ -1,31 +1,28 @@
-import os
 import grpc
 from concurrent import futures
 import hello_pb2
 import hello_pb2_grpc
 import time
 import sqlite3
-import sys
-
-print("server.py - Script started", file=sys.stderr)
-
+print("server.py")
 class Greeter(hello_pb2_grpc.GreeterServicer):
     def __init__(self):
-        print("Initializing Greeter", file=sys.stderr)
         self.conn = sqlite3.connect('nombre.db', check_same_thread=False)
+
         self.cursor = self.conn.cursor()
-        print("Database connected", file=sys.stderr)
         
     def SayHello(self, request, context):
         name = request.name
-        print(f"Received request to SayHello to {name}", file=sys.stderr)
+        print(f"Received request to SayHello to {name}")
         
+        # Buscar el mensaje personalizado en la base de datos
         self.cursor.execute('SELECT mensaje FROM saludos WHERE nombre = ?', (name,))
         result = self.cursor.fetchone()
         
         if result:
             message = result[0]
         else:
+            # Si no se encuentra un mensaje personalizado, usar el mensaje por defecto
             self.cursor.execute('SELECT mensaje FROM saludos WHERE nombre = "Default"')
             result = self.cursor.fetchone()
             message = result[0] if result else f"Hello, {name}!"
@@ -33,20 +30,17 @@ class Greeter(hello_pb2_grpc.GreeterServicer):
         return hello_pb2.HelloReply(message=message)
 
 def serve():
-    default_port = '50051'
-    port = os.environ.get('PORT', default_port)
-    print(f"Environment variable PORT: {os.environ.get('PORT')}", file=sys.stderr)
-    print(f"Default port: {default_port}", file=sys.stderr)
-    print(f"Selected port: {port}", file=sys.stderr)
-    
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     hello_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port(f'[::]:{port}')
-    print(f"Server started. Listening on port {port}.", file=sys.stderr)
+    server.add_insecure_port('[::]:50051')
+    print("Server started. Listening on port 50051.")
     server.start()
-    print("Server is running. Waiting for termination.", file=sys.stderr)
-    server.wait_for_termination()
+    try:
+        while True:
+            time.sleep(86400)  # Sleep for a day
+    except KeyboardInterrupt:
+        server.stop(0)
+        print("Server stopped.")
 
 if __name__ == '__main__':
-    print("Main block entered", file=sys.stderr)
-    serve()
+    serve(debug=True)
