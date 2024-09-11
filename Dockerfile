040d3dd60a3926d8c61834892e9865c2dc164e9a
santish1802 <santiago497045@gmail.com>
2024-09-11 17:40:40 -0500

    5

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
-# Usar la imagen oficial de Python
-FROM python:3.10-slim
+FROM python:3.9
 
-# Establecer el directorio de trabajo en el contenedor
 WORKDIR /app
 
-# Copiar los archivos del proyecto al directorio de trabajo
-COPY . /app
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
-# Instalar las dependencias necesarias (gRPC, Protobuf y SQLite)
-RUN pip install --no-cache-dir grpcio grpcio-tools protobuf
+COPY . .
 
-# Exponer el puerto 50051 para gRPC
-EXPOSE 50051
-
-# Comando para iniciar el servidor gRPC
 CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -1,28 +1,28 @@
+import os
 import grpc
 from concurrent import futures
 import hello_pb2
 import hello_pb2_grpc
 import time
 import sqlite3
+
 print("server.py")
+
 class Greeter(hello_pb2_grpc.GreeterServicer):
     def __init__(self):
         self.conn = sqlite3.connect('nombre.db', check_same_thread=False)
-
         self.cursor = self.conn.cursor()
         
     def SayHello(self, request, context):
         name = request.name
         print(f"Received request to SayHello to {name}")
         
-        # Buscar el mensaje personalizado en la base de datos
         self.cursor.execute('SELECT mensaje FROM saludos WHERE nombre = ?', (name,))
         result = self.cursor.fetchone()
         
         if result:
             message = result[0]
         else:
-            # Si no se encuentra un mensaje personalizado, usar el mensaje por defecto
             self.cursor.execute('SELECT mensaje FROM saludos WHERE nombre = "Default"')
             result = self.cursor.fetchone()
             message = result[0] if result else f"Hello, {name}!"
@@ -30,17 +30,13 @@ class Greeter(hello_pb2_grpc.GreeterServicer):
         return hello_pb2.HelloReply(message=message)
 
 def serve():
+    port = os.environ.get('PORT', '50051')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     hello_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:50051')
-    print("Server started. Listening on port 50051.")
+    server.add_insecure_port(f'[::]:{port}')
+    print(f"Server started. Listening on port {port}.")
     server.start()
-    try:
-        while True:
-            time.sleep(86400)  # Sleep for a day
-    except KeyboardInterrupt:
-        server.stop(0)
-        print("Server stopped.")
+    server.wait_for_termination()
 
 if __name__ == '__main__':
     serve()
