ab2c33a7e1d6962762861ad3027017694aaad039
santish1802 <santiago497045@gmail.com>
2024-09-11 19:35:59 -0500

    final casi

--- prob.py
+++ prob.py
@@ -3,11 +3,11 @@ import hello_pb2
 import hello_pb2_grpc
 
 def run():
-    with grpc.insecure_channel('junction.proxy.rlwy.net:18083') as channel:
+    with grpc.insecure_channel('junction.proxy.rlwy.net:23876') as channel:
         stub = hello_pb2_grpc.GreeterStub(channel)
         
         # Lista de nombres para probar
-        names = ['Alice', 'Bob', 'Charlie', 'xd']
+        names = ['Alice', 'Bob', 'Charlie', 'Jeremy', 'Santiago']
         
         for name in names:
             response = stub.SayHello(hello_pb2.HelloRequest(name=name))
--- server.py
+++ server.py
@@ -7,18 +7,18 @@ import time
 import sqlite3
 import sys
 
-print("server.py - Script started", file=sys.stderr)
+print("server.py - Script started", file=sys.stdout)
 
 class Greeter(hello_pb2_grpc.GreeterServicer):
     def __init__(self):
-        print("Initializing Greeter", file=sys.stderr)
+        print("Initializing Greeter", file=sys.stdout)
         self.conn = sqlite3.connect('nombre.db', check_same_thread=False)
         self.cursor = self.conn.cursor()
-        print("Database connected", file=sys.stderr)
+        print("Database connected", file=sys.stdout)
         
     def SayHello(self, request, context):
         name = request.name
-        print(f"Received request to SayHello to {name}", file=sys.stderr)
+        print(f"Received request to SayHello to {name}", file=sys.stdout)
         
         self.cursor.execute('SELECT mensaje FROM saludos WHERE nombre = ?', (name,))
         result = self.cursor.fetchone()
@@ -35,18 +35,18 @@ class Greeter(hello_pb2_grpc.GreeterServicer):
 def serve():
     default_port = '50051'
     port = os.environ.get('PORT', default_port)
-    print(f"Environment variable PORT: {os.environ.get('PORT')}", file=sys.stderr)
-    print(f"Default port: {default_port}", file=sys.stderr)
-    print(f"Selected port: {port}", file=sys.stderr)
+    print(f"Environment variable PORT: {os.environ.get('PORT')}", file=sys.stdout)
+    print(f"Default port: {default_port}", file=sys.stdout)
+    print(f"Selected port: {port}", file=sys.stdout)
     
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     hello_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port(f'[::]:{port}')
-    print(f"Server started. Listening on port {port}.", file=sys.stderr)
+    print(f"Server started. Listening on port {port}.", file=sys.stdout)
     server.start()
-    print("Server is running. Waiting for termination.", file=sys.stderr)
+    print("Server is running. Waiting for termination.", file=sys.stdout)
     server.wait_for_termination()
 
 if __name__ == '__main__':
-    print("Main block entered", file=sys.stderr)
+    print("Main block entered", file=sys.stdout)
     serve()
